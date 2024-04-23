89c4c7b8033667f4e1dba8afa4a8e54d66a6adf1
sashakatne <yashaswi.katne99@gmail.com>
2024-04-23 11:42:45 -0600

    removed vendor ram

--- async_fifo2.sv
+++ async_fifo2.sv
@@ -28,15 +28,10 @@ module fifomem (rdata, wdata, waddr, raddr, wclken, wclk);
     input [ADDRSIZE-1:0] waddr, raddr;
     input wclken, wclk;
 
-    `ifdef VENDORRAM
-    // instantiation of a vendor's dual-port RAM
-    VENDOR_RAM MEM (.dout(rdata), .din(wdata), .waddr(waddr), .raddr(raddr), .wclken(wclken), .clk(wclk));
-    `else
     reg [DATASIZE-1:0] MEM [0:DEPTH-1];
     assign rdata = MEM[raddr];
     always @(posedge wclk)
-    if (wclken) MEM[waddr] <= wdata;
-    `endif
+        if (wclken) MEM[waddr] <= wdata;
 
 endmodule
 
